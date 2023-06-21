5223fefb69a7ca79df6b5f875b71c7d6cd7ae0e6
espace2021 <92257192+espace2021@users.noreply.github.com>
2023-06-21 11:17:18 +0200

    Update categorie.route.js

--- routes/categorie.route.js
+++ routes/categorie.route.js
@@ -1,13 +1,11 @@
-var express = require('express');
-var router = express.Router();
-
-// Créer une instance de categorie.
-const Categorie = require('../models/categorie');
+const express = require('express');
+const router = express.Router();
+const Categorie=require("../models/categorie")
 
 // afficher la liste des categories.
 router.get('/', async (req, res, )=> {
     try {
-        const cat = await Categorie.find();
+        const cat = await Categorie.find({}, null, {sort: {'_id': -1}})
                 
         res.status(200).json(cat);
     } catch (error) {
@@ -16,18 +14,20 @@ router.get('/', async (req, res, )=> {
 
 });
 // créer un nouvelle catégorie
-router.post('/', async (req, res) =>  {
+router.post('/',async (req, res) =>  {
     const { nomcategorie, imagecategorie} = req.body;
     const newCategorie = new Categorie({nomcategorie:nomcategorie, imagecategorie:imagecategorie})
     try {
+
         await newCategorie.save();
+
         res.status(200).json(newCategorie );
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
 
-});
 
+});
 // chercher une catégorie 
 router.get('/:categorieId',async(req, res)=>{
     try {
@@ -38,30 +38,28 @@ router.get('/:categorieId',async(req, res)=>{
         res.status(404).json({ message: error.message });
     }
 });
-
-
 // modifier une catégorie
 router.put('/:categorieId', async (req, res)=> {
-    const { nomcategorie, imagecategorie} = req.body;
-    const id  = req.params.categorieId;
 
     try {
-    
-    const cat1 = { nomcategorie:nomcategorie,imagecategorie:imagecategorie, _id:id };
-console.log(cat1)
-    await Categorie.findByIdAndUpdate(id, cat1);
+  
+    const cat1 = await Categorie.findByIdAndUpdate(
+        req.params.categorieId,
+        { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(cat1);
 
-    res.json(cat1);
     } catch (error) {
     res.status(404).json({ message: error.message });
     }
 });
-
 // Supprimer une catégorie
 router.delete('/:categorieId', async (req, res)=> {
     const  id  = req.params.categorieId;
     await Categorie.findByIdAndDelete(id);
-    res.json({ message: "categorie deleted successfully." });
-});
 
+    res.json({ message: "categorie deleted successfully." });
+
+});
 module.exports = router;
